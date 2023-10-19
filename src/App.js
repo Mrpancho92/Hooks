c762382eab28cd3a0675b698a7411abd506c9d2d
Pavel <133679525+Mrpancho92@users.noreply.github.com>
2023-10-19 13:44:16 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
+import {Component, useState, useEffect} from 'react'; 
+import Container from 'react-bootstrap/Container';
 import './App.css';
+
+// class Slider extends Component {
+
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             autoplay: false,
+//             slide: 0
+//         }
+//     }
+
+//     componentDidMount() {
+//         document.title = `Slide: ${this.state.slide}`;
+//     }
+
+//     componentDidUpdate() {
+//         document.title = `Slide: ${this.state.slide}`;
+//     }
+
+
+//     changeSlide = (i) => {
+//         this.setState(({slide}) => ({
+//             slide: slide + i
+//         }))
+//     }
+
+//     toggleAutoplay = () => {
+//         this.setState(({autoplay}) => ({
+//             autoplay: !autoplay
+//         }))
+//     }
+
+//     render() {
+//         return (
+//             <Container>
+//                 <div className="slider w-50 m-auto">
+//                     <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+//                     <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
+//                     <div className="buttons mt-3">
+//                         <button 
+//                             className="btn btn-primary me-2"
+//                             onClick={() => this.changeSlide(-1)}>-1</button>
+//                         <button 
+//                             className="btn btn-primary me-2"
+//                             onClick={() => this.changeSlide(1)}>+1</button>
+//                         <button 
+//                             className="btn btn-primary me-2"
+//                             onClick={this.toggleAutoplay}>toggle autoplay</button>
+//                     </div>
+//                 </div>
+//             </Container>
+//         )
+//     }
+// }
+
+
+const Slider = (props) => {
+
+    const [slide , setSlide] = useState(0);
+    const [autoplay, setAutoplay] = useState(false);
+
+
+    useEffect(()=> {
+        document.title = `Slide: ${slide}`;
+    });
+        
+    function changeSlide(i) {
+        setSlide( slide => slide + i);
+    }
+
+    const toggleAutoplay = () => {
+        setAutoplay(autoplay => !autoplay);
+    }
+    return (
+        <Container>
+            <div className="slider w-50 m-auto">
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {slide} <br/>{autoplay ? 'auto' : null}</div>
+                <div className="buttons mt-3">
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={ () => changeSlide(-1)}>-1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={ () => changeSlide(1)}>+1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={toggleAutoplay}>toggle autoplay</button>
+                </div>
+            </div>
+        </Container>
+    )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Slider/>
   );
 }
 
 export default App;
+
+
+
+// const details = {
+//     1: {name:'peperoni', price: 200},
+//     2: {name:'meat', price: 300},
+//     3: {name:'vegan', price: 400},
+// }
+
+// const pizza = Array.from({length: 3}, (_, i) => (`${details[i+1].name}`));
